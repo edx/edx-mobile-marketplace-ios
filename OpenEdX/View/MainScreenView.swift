89e92c0b901330813a9f66803a918479a293554a
Vadim Kuznetsov <vadim@touchappmedia.com>
2024-07-29 22:26:33 +0300

    chore: fix settings button for iOS 15

--- OpenEdX/View/MainScreenView.swift
+++ OpenEdX/View/MainScreenView.swift
@@ -188,7 +188,9 @@ struct MainScreenView: View {
         }
         .accentColor(Theme.Colors.accentXColor)
         .introspect(.viewController, on: .iOS(.v15)) { controller in
-            controller.navigationController?.setNavigationBarHidden(true, animated: false)
+            if viewModel.selection != .profile {
+                controller.navigationController?.setNavigationBarHidden(true, animated: false)
+            }
         }
     }
     
