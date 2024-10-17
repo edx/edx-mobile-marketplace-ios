396397609b7b38fe5acbf7aeb5929d55266bd46f
Saeed Bashir <sbashir@edx.org>
2024-10-17 15:49:11 +0500

    chore: make buttons corner radius configureable via config file

--- Theme/Theme/Theme.swift
+++ Theme/Theme/Theme.swift
@@ -250,6 +250,7 @@ public struct Theme {
     
     public struct Shapes {
         public static var isRoundedCorners: Bool = true
+        public static var buttonCornersRadius: Double = 8.0
         public static let screenBackgroundRadius = 24.0
         public static let cardImageRadius = 10.0
         public static let textInputShape =  {
@@ -257,7 +258,7 @@ public struct Theme {
             return RoundedRectangle(cornerRadius: radius)
         }()
         public static let buttonShape = {
-            let radius: CGFloat = isRoundedCorners ? 8 : 0
+            let radius: CGFloat = isRoundedCorners ? buttonCornersRadius : 0
             return RoundedCorners(tl: radius, tr: radius, bl: radius, br: radius)
         }()
         public static let unitButtonShape = RoundedCorners(tl: 21, tr: 21, bl: 21, br: 21)
