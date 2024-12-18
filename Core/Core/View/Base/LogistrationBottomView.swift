dfbe1880d848412ac693eb290a2f6489a040bfab
Anton Yarmolenko <37253+rnr@users.noreply.github.com>
2024-12-18 16:55:35 +0100

    chore: transfer post login data through WhatNew screen

--- Core/Core/View/Base/LogistrationBottomView.swift
+++ Core/Core/View/Base/LogistrationBottomView.swift
@@ -26,6 +26,16 @@ public enum LogistrationAction {
     case register
 }
 
+public struct PostLoginData: Sendable {
+    public let authMethod: String
+    public var showSocialRegisterBanner: Bool
+    
+    public init(authMethod: String, showSocialRegisterBanner: Bool) {
+        self.authMethod = authMethod
+        self.showSocialRegisterBanner = showSocialRegisterBanner
+    }
+}
+
 public struct LogistrationBottomView: View {
     private let action: (LogistrationAction) -> Void
     
