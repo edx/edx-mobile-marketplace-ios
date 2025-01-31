bdacf44553bdb0920f7d66662317326d0c5d837b
Tayyab Akram <tayyab.akram@arbisoft.com>
2025-01-31 12:11:36 +0500

    feat: write pagination manger to reuse data loading logic

---
+++ Core/Core/Network/Pagination/PaginationTask.swift
@@ -0,0 +1,51 @@
+//
+//  PaginationTask.swift
+//  Core
+//
+//  Created by Muhammad Tayyab  Akram on 1/30/25.
+//
+
+import Foundation
+
+/// A structure representing an ongoing pagination task.
+public struct PaginationTask<Item>: Equatable {
+    typealias Operation = (PaginationTask<Item>) async throws -> [Item]
+    
+    private let task: Task<[Item], Error>
+    
+    private init(task: Task<[Item], Error>) {
+        self.task = task
+    }
+
+    static func create(operation: @escaping Operation) -> PaginationTask<Item> {
+        var paginationTask: PaginationTask<Item>!
+        paginationTask = PaginationTask(
+            task: Task { try await operation(paginationTask) }
+        )
+        return paginationTask
+    }
+    
+    /// Cancels the ongoing pagination task.
+    public func cancel() {
+        task.cancel()
+    }
+    
+    /// A Boolean value that indicates whether the pagination task was canceled.
+    public var isCancelled: Bool {
+        return task.isCancelled
+    }
+    
+    /// Returns the fetched items asynchronously, throwing an error if the task fails.
+    public var items: [Item] {
+        get async throws {
+            return try await task.value
+        }
+    }
+    
+    /// Returns the result of the task asynchronously as a `Result` type.
+    public var result: Result<[Item], Error> {
+        get async {
+            return await task.result
+        }
+    }
+}
