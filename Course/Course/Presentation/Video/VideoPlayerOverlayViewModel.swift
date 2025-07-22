3e576013d258e56fc9e9681f7f4e47ce356aa668
Muhammad Tayyab Akram <takram@2u.com>
2025-07-22 14:27:37 +0500

    feat: add tap & hold to play at 2x speed in video player (#159)

---
+++ Course/Course/Presentation/Video/VideoPlayerOverlayViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  VideoPlayerOverlayViewModel.swift
+//  Course
+//
+//  Created by Muhammad Tayyab  Akram on 7/14/25.
+//
+
+import AVFoundation
+
+final class VideoPlayerOverlayViewModel: ObservableObject {
+    @Published var showSpeedOverlay = false
+}
