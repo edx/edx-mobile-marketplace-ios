5de422414325e179a4d83e4fa77abd7242556552
Sumanta Roy <sroytapadar-sonata@2u.com>
2026-01-01 15:17:39 +0530

    fix: Mapping custom dimensions on firebase.

--- Core/Core/CourseUpgrade/ViewModels/UpgradeInfoViewModel.swift
+++ Core/Core/CourseUpgrade/ViewModels/UpgradeInfoViewModel.swift
@@ -216,7 +216,7 @@ public class UpgradeInfoViewModel: ObservableObject {
     
     func trackOnCertificatePreviewPurchased() {
         var props = certificatePreviewProps()
-        props[CertPreviewPropertyKey.attemptsToPurchase.rawValue] = certificatePreviewExperimentManager.attemptsSinceLastCertificatePreviewAndReset(forCourseId: courseID)
+        props[CertPreviewPropertyKey.attemptsToPurchase.rawValue] = "\(certificatePreviewExperimentManager.attemptsSinceLastCertificatePreviewAndReset(forCourseId: courseID))"
         certificatePreviewExperimentManager.trackEvent(
             CertPreviewEventKey.certPreviewPurchased.rawValue,
             properties: props
@@ -231,9 +231,13 @@ public class UpgradeInfoViewModel: ObservableObject {
         let props: [String: Any] = [
             CertPreviewPropertyKey.courseId.rawValue: courseID,
             CertPreviewPropertyKey.price.rawValue: lmsPrice,
-            CertPreviewPropertyKey.showCertificatePreview.rawValue: shouldShowCertificatePreview(),
+            CertPreviewPropertyKey.showCertificatePreview.rawValue: boolToString(shouldShowCertificatePreview()),
             CertPreviewPropertyKey.certPreviewVariant.rawValue: shouldShowCertificatePreview() ? CertPreviewPropertyKey.treatment.rawValue : CertPreviewPropertyKey.control.rawValue
         ]
         return props
+    }
+        
+    private func boolToString(_ value: Bool) -> String {
+        return value ? "true" : "false"
     }
 }
